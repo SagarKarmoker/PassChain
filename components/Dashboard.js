b152f2270ab6fac07f1e3cc5672dfc165686385e
Sagar-Karmoker <sagarkarmoker2001@gmail.com>
2024-01-07 17:17:23 +0600

    Destroy Vault done and Blockchain Based password Manager v.1.2.0 Done ✅

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -11,27 +11,38 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { toast } from "sonner"
-import { Separator } from "@/components/ui/separator"
+import { toast } from "sonner";
+import { Separator } from "@/components/ui/separator";
 import PassCard from "./PassCard";
 
-import { useContract, useContractWrite, useContractRead, useAddress, useSigner, Web3Button } from "@thirdweb-dev/react";
+import {
+    useContract,
+    useContractWrite,
+    useContractRead,
+    useAddress,
+    useSigner,
+    Web3Button,
+} from "@thirdweb-dev/react";
 
-const ethers = require('ethers');
-import CryptoJS from 'crypto-js';
-
+const ethers = require("ethers");
+import CryptoJS from "crypto-js";
 
 function Dashboard() {
     const address = useAddress();
-    const { contract } = useContract("0x35649F537164f13935a1A80Da9eCd922C6dC4Cf8");
+    const { contract } = useContract(
+        "0x35649F537164f13935a1A80Da9eCd922C6dC4Cf8"
+    );
     const { data } = useContractRead(contract, "getVaultAddr", [address]);
-    const { mutateAsync: addPass, isLoading } = useContractWrite(contract, "addPass")
+    const { mutateAsync: addPass, isLoading } = useContractWrite(
+        contract,
+        "addPass"
+    );
 
-    const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [url, setURL] = useState('');
-    const [key, setKey] = useState('');
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [url, setURL] = useState("");
+    const [key, setKey] = useState("");
     const [vaultData, setVaultData] = useState([]);
 
     const handleSavePass = async () => {
@@ -41,16 +52,18 @@ function Dashboard() {
             const _password = CryptoJS.AES.encrypt(password, key).toString();
             const _url = CryptoJS.AES.encrypt(url, key).toString();
 
-            const addPassData = await addPass({ args: [_username, _email, _password, _url] });
+            const addPassData = await addPass({
+                args: [_username, _email, _password, _url],
+            });
             console.info("contract call successs", addPassData);
-            toast("🎉Saved successfully")
+            toast("🎉Saved successfully");
         } catch (err) {
-            toast("⚠ Failed to save password")
+            toast("⚠ Failed to save password");
             console.error("contract call failure", err);
         }
-    }
+    };
 
-    // getvault 
+    // getvault
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
     // Assuming you have the contract ABI and address
@@ -65,7 +78,11 @@ function Dashboard() {
 
         const contractABI = contract.abi;
         // Create a contract instance
-        const mycontract = new ethers.Contract(contractAddress, contractABI, signer);
+        const mycontract = new ethers.Contract(
+            contractAddress,
+            contractABI,
+            signer
+        );
 
         // Call the contract function
         try {
@@ -76,6 +93,19 @@ function Dashboard() {
         }
     }
 
+    const { mutateAsync: destoryVault } = useContractWrite(
+        contract,
+        "destoryVault"
+    );
+    const handleDestory = async () => {
+        try {
+            const data = await destoryVault({ args: [] });
+            console.info("contract call successs", data);
+        } catch (err) {
+            console.error("contract call failure", err);
+        }
+    };
+
     const getMyKey = async () => {
         // Check if the contract object is defined
         if (!contract || !contract.abi) {
@@ -85,7 +115,11 @@ function Dashboard() {
 
         const contractABI = contract.abi;
         // Create a contract instance
-        const mycontract = new ethers.Contract(contractAddress, contractABI, signer);
+        const mycontract = new ethers.Contract(
+            contractAddress,
+            contractABI,
+            signer
+        );
 
         // Call the contract function
         try {
@@ -94,12 +128,12 @@ function Dashboard() {
         } catch (error) {
             console.error(error);
         }
-    }
+    };
 
     useEffect(() => {
         getMyKey();
         getVault();
-    }, [getMyKey])
+    }, [getMyKey]);
 
     return (
         <div className="flex mt-4">
@@ -116,23 +150,55 @@ function Dashboard() {
                         <div className="grid w-full items-center gap-4">
                             <div className="flex flex-col space-y-1.5">
                                 <Label htmlFor="username">Username</Label>
-                                <Input id="username" name="username" placeholder="Username" onChange={(e) => setUsername(e.target.value)} />
+                                <Input
+                                    id="username"
+                                    name="username"
+                                    placeholder="Username"
+                                    onChange={(e) => setUsername(e.target.value)}
+                                />
 
                                 <Label htmlFor="email">Email Address</Label>
-                                <Input id="email" name="email" placeholder="Email Address" onChange={(e) => setEmail(e.target.value)} />
+                                <Input
+                                    id="email"
+                                    name="email"
+                                    placeholder="Email Address"
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
 
                                 <Label htmlFor="password">Password</Label>
-                                <Input id="password" name="password" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+                                <Input
+                                    id="password"
+                                    name="password"
+                                    type="password"
+                                    placeholder="Password"
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
 
                                 <Label htmlFor="URL">URL</Label>
-                                <Input id="URL" name="URL" placeholder="URL" onChange={(e) => setURL(e.target.value)} />
+                                <Input
+                                    id="URL"
+                                    name="URL"
+                                    placeholder="URL"
+                                    onChange={(e) => setURL(e.target.value)}
+                                />
                             </div>
                         </div>
                     </CardContent>
                     <CardFooter className="flex justify-end">
-                        <Button type="submit" onClick={handleSavePass}>Save Password</Button>
+                        <Button type="submit" onClick={handleSavePass}>
+                            Save Password
+                        </Button>
                     </CardFooter>
                 </Card>
+
+                {/* destory */}
+                <Button
+                    className="w-full mt-5"
+                    variant="destructive"
+                    onClick={handleDestory}
+                >
+                    Destroy Vault
+                </Button>
             </div>
             <div className="w-1/10 pl-3 pr-3">
                 {/* Content for the right 10% width */}
@@ -141,13 +207,20 @@ function Dashboard() {
             <div className="w-6/10">
                 {/* Content for the right 60% width */}
                 <div className="grid grid-cols-3 gap-4">
-                    {vaultData.map((item, index) => (
-                        item[0] != "" && <PassCard cardIndex={index} dataFromParent={item} vaultAddr={data} myKey={key} />
-                    ))}
+                    {vaultData.map(
+                        (item, index) =>
+                            item[0] != "" && (
+                                <PassCard
+                                    cardIndex={index}
+                                    dataFromParent={item}
+                                    vaultAddr={data}
+                                    myKey={key}
+                                />
+                            )
+                    )}
                 </div>
             </div>
         </div>
-    )
+    );
 }
-
-export default Dashboard
+export default Dashboard;
